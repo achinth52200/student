1111e7c67857a52423d0bd8fe129f2d9003940e3
Achinth .M <achinthmicrosoft@gmail.com>
2025-09-07 12:31:30 +0000

    we should eneter our mental health it should suggest  and give tips as b

--- src/app/well-being/page.tsx
+++ src/app/well-being/page.tsx
@@ -1,6 +1,7 @@
 import { AppHeader } from "@/components/app-header";
 import { AppSidebar } from "@/components/app-sidebar";
 import { WellbeingChat } from "@/components/dashboard/wellbeing-chat";
+import { WellbeingSupport } from "@/components/dashboard/wellbeing-support";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 export default function WellbeingPage() {
@@ -11,7 +12,10 @@ export default function WellbeingPage() {
         <SidebarInset className="flex-1">
           <AppHeader />
           <main className="p-4 sm:p-6 lg:p-8">
-            <WellbeingChat />
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-start">
+              <WellbeingChat />
+              <WellbeingSupport />
+            </div>
           </main>
         </SidebarInset>
       </div>
