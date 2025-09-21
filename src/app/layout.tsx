2da034e9cb32657b834b82f9443656adc07934ce
Achinth .M <achinthmicrosoft@gmail.com>
2025-09-21 11:26:57 +0000

    make full glass effect like this shown in figure sidebar component and e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,9 +38,7 @@ export default function RootLayout({
         />
       </head>
       <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
-        <div className="absolute inset-0 -z-10 h-full w-full bg-background bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-          <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-primary/20 opacity-20 blur-[100px]"></div>
-        </div>
+         <div className="absolute top-0 -z-10 h-full w-full bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
         <LoaderProvider>
             <AuthProvider>
               <ReminderProvider>
