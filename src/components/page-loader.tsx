9c466382d917978e18462b4e6e366fd92d0452ec
Achinth .M <achinthmicrosoft@gmail.com>
2025-10-05 16:53:41 +0000

    screen loading which i gave snippt use that

--- src/components/page-loader.tsx
+++ src/components/page-loader.tsx
@@ -12,17 +12,13 @@ export function PageLoader() {
 
   return (
     <div className="page-loader-overlay">
-      <div className="loader-wrapper">
-        <span style={{'--i': 1} as React.CSSProperties}>L</span>
-        <span style={{'--i': 2} as React.CSSProperties}>O</span>
-        <span style={{'--i': 3} as React.CSSProperties}>A</span>
-        <span style={{'--i': 4} as React.CSSProperties}>D</span>
-        <span style={{'--i': 5} as React.CSSProperties}>I</span>
-        <span style={{'--i': 6} as React.CSSProperties}>N</span>
-        <span style={{'--i': 7} as React.CSSProperties}>G</span>
-        <span style={{'--i': 8} as React.CSSProperties}>.</span>
-        <span style={{'--i': 9} as React.CSSProperties}>.</span>
-        <span style={{'--i': 10} as React.CSSProperties}>.</span>
+       <div className="atom-spinner">
+        <div className="spinner-inner">
+          <div className="spinner-line"></div>
+          <div className="spinner-line"></div>
+          <div className="spinner-line"></div>
+          <div className="spinner-circle">&#9679;</div>
+        </div>
       </div>
     </div>
   );
